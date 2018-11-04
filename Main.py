aa7d0426e525b3723a5f430f4556bed51825418d
Chimaek <pipiru100@gmail.com>
2018-11-04 17:59:06 +0900

    dssd

--- Main.py
+++ Main.py
@@ -1,7 +1,13 @@
 import pandas as pd
+import numpy as np
+dataX,dataY=[],[]
 
-data=pd.read_csv("data.csv","w",encoding="utf-8",)
+#csv파일에서 읽어와 빈값을 지우고 배열로 변환시킴문자열을 숫자로
+data=pd.read_csv("data.csv","r").dropna(axis=1,how='any').drop("BTC")
 
 
-print(data.head())
+print(data)
+
+
+
 
