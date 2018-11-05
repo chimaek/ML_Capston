3a036679ed4a2f39acb7d0dfe76e4856944c7182
Chimaek <pipiru100@gmail.com>
2018-11-05 23:57:04 +0900

    ㄴㅇㄴ

--- Main.py
+++ Main.py
@@ -3,11 +3,7 @@ import numpy as np
 dataX,dataY=[],[]
 
 #csv파일에서 읽어와 넘파일 배열로 변환
-getData=pd.read_csv("data.csv","r",encoding='utf-8',sep=",")
-
-
-
-
+getData=pd.read_csv("data.csv","r",encoding='utf-8',quotechar=",").values
 print(getData)
 
 
--- get.py
+++ get.py
@@ -17,7 +17,7 @@ def Cycle(second=300):
 
     # json파일을 받아와 데이터프레임으로 가공합니다.
     json = pd.read_json(readJson)
-    p1 = pd.DataFrame(json,dtype=int).T.drop(["status"]).drop(["24H_fluctate"], axis=1).drop(['24H_fluctate_rate'], axis=1) \
+    p1 = pd.DataFrame(json,dtype=int,).T.drop(["status"]).drop(["24H_fluctate"], axis=1).drop(['24H_fluctate_rate'], axis=1) \
         .drop(['average_price'], axis=1).drop(['buy_price'], axis=1).drop(['closing_price'], axis=1) \
         .drop(['opening_price'], axis=1).drop(['units_traded'], axis=1).drop(['volume_1day'], axis=1) \
         .drop(['volume_7day'], axis=1).drop(['sell_price'], axis=1).drop(["date"], axis=1)
